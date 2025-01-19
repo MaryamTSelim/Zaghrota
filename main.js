b8e66f4dbcf7120efd691d5cd577f6d0324c0144
Maryam Selim <maryamtselim@gmail.com>
2025-01-20 00:03:30 +0200

    init

---
+++ main.js
@@ -0,0 +1,21 @@
+window.addEventListener("load", (windowLoadEvent) => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    document.getElementById("bride-name").innerText = urlParams.get("name");
+
+    document.getElementById("audio-start").addEventListener('ended', function() {
+        this.currentTime = 0;
+        this.play();
+    }, false);
+
+
+    document.getElementById("zagrat").addEventListener("mousedown",(e)=>{
+        document.getElementById("audio-start").currentTime = 0;
+        document.getElementById("audio-start").play();
+    })
+    document.getElementById("zagrat").addEventListener("mouseup",(e)=>{
+        document.getElementById("audio-start").pause();
+        document.getElementById("audio-start").currentTime = 0;
+        document.getElementById("audio-end").play();
+    })
+});
